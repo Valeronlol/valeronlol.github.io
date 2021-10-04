244c5495771dcb73df2d3d1c3997f63c617255d4
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2021-10-04 16:42:38 +0300

    added favicon

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,8 @@ import { writable } from 'svelte/store';
 
 const lang = writable()
 const theme = writable()
-const currentPageTitle = writable()
 
 export {
     lang,
     theme,
-    currentPageTitle,
 }
