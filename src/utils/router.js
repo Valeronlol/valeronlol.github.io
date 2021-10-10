79044fa185d5c2326b99265ac75befcaad406ec9
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2021-10-10 12:26:11 +0300

    remove logs

--- src/utils/router.js
+++ src/utils/router.js
@@ -3,7 +3,6 @@ import routes from './routes.json'
 const isCurrentPath = path => path === window.location.pathname
 
 const getCurrentRoute = currentPath => {
-    console.log('CONSOLE: currentPath', currentPath);
     if (currentPath.endsWith('/') && currentPath !== '/') {
         currentPath = currentPath.slice(0, -1)
     }
