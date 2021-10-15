c814271db68e1b4d16b42e9318285f8d95fea15b
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2021-10-15 23:18:51 +0300

    path clear fix

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,13 +1,18 @@
 import routes from './routes.json'
 
+const clearPath = (path) => {
+    if (path.endsWith('/') && path !== '/') {
+        return path.slice(0, -1)
+    }
+
+    return path
+}
+
 const isCurrentPath = path => path === window.location.pathname
 
 const getCurrentRoute = currentPath => {
-    if (currentPath.endsWith('/') && currentPath !== '/') {
-        currentPath = currentPath.slice(0, -1)
-    }
-
-    return routes.find(route => route.target === currentPath)
+    const clearPath = clearPath(currentPath)
+    routes.find(route => clearPath(route.target) === clearPath)
 }
 
 const routesWithoutMain = routes.filter(({ target }) => target !== '/')
@@ -16,4 +21,5 @@ export {
     routesWithoutMain as routes,
     isCurrentPath,
     getCurrentRoute,
+    clearPath,
 }
