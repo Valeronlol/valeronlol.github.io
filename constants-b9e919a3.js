cf7ece4bb8c0337abcab41d56b8aca751287bc31
Valerii_Kuzivanov <valerii.kuzivanov@ridewithvia.com>
2021-11-03 20:32:25 +0300

    deploy

---
+++ constants-b9e919a3.js
@@ -0,0 +1,1 @@
+const a="app",s="lang",t="theme",e={DEFAULT:"default",DARK:"dark"};export{a,s as b,t as s,e as t};
---
+++ languages-221bcc59.js
@@ -0,0 +1,1 @@
+const e=["en","ru"],r=e.reduce(((e,r)=>(e[r]=r,e)),{});export{e as l,r as m};
